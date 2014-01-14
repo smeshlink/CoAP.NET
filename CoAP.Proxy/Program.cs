947832ee2f141e62ea67212b334ce3f5330f9200
longshine <longxianghe@gmail.com>
2014-01-15 01:18:05 +0800

    proxy sample

---
+++ CoAP.Proxy/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CoAP.EndPoint;
+
+namespace CoAP.Examples
+{
+    class Program
+    {
+#if COAPALL
+        static ISpec Spec = CoAP.Spec.Draft13;
+#endif
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                ProxyEndpoint proxy = new ProxyEndpoint();
+                Console.WriteLine("CoAP server [{1}] is listening on port {0}.",
+                    proxy.Communicator.Port,
+                    Spec.Name);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
