09ef5453aefc60eed7c68b43dfebe73030101698
longshine <longxianghe@gmail.com>
2014-06-03 13:49:55 +0800

    make CoapStack subclass of LayerStack

--- CoAP.NET/Stack/CoapStack.cs
+++ CoAP.NET/Stack/CoapStack.cs
@@ -10,6 +10,7 @@
  */
 
 using CoAP.Net;
+using CoAP.Server;
 
 namespace CoAP.Stack
 {
@@ -17,51 +18,14 @@ namespace CoAP.Stack
     /// Builds up the stack of CoAP layers
     /// that process the CoAP protocol.
     /// </summary>
-    public class CoapStack
+    public class CoapStack : LayerStack
     {
-        private LayerStack _stack = new LayerStack();
-
         public CoapStack(ICoapConfig config)
         {
-            _stack.AddLast("Observe", new ObserveLayer(config));
-            _stack.AddLast("Blockwise", new BlockwiseLayer(config));
-            _stack.AddLast("Token", new TokenLayer(config));
-            _stack.AddLast("Reliability", new ReliabilityLayer(config));
-        }
-
-        public LayerStack Layers
-        {
-            get { return _stack; }
-        }
-
-        public void SendRequest(Request request)
-        {
-            _stack.SendRequest(request);
-        }
-
-        public void SendResponse(Exchange exchange, Response response)
-        {
-            _stack.SendResponse(exchange, response);
-        }
-
-        public void SendEmptyMessage(Exchange exchange, EmptyMessage message)
-        {
-            _stack.SendEmptyMessage(exchange, message);
-        }
-
-        public void ReceiveRequest(Exchange exchange, Request request)
-        {
-            _stack.ReceiveRequest(exchange, request);
-        }
-
-        public void ReceiveResponse(Exchange exchange, Response response)
-        {
-            _stack.ReceiveResponse(exchange, response);
-        }
-
-        public void ReceiveEmptyMessage(Exchange exchange, EmptyMessage message)
-        {
-            _stack.ReceiveEmptyMessage(exchange, message);
+            this.AddLast("Observe", new ObserveLayer(config));
+            this.AddLast("Blockwise", new BlockwiseLayer(config));
+            this.AddLast("Token", new TokenLayer(config));
+            this.AddLast("Reliability", new ReliabilityLayer(config));
         }
     }
 }
