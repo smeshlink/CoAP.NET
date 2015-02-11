15c80bed39e2b03533cbb9f2cb87cfb46a6f8dfe
longshine <longxianghe@gmail.com>
2015-02-11 12:23:23 +0800

    prepare for v1.0.0

--- CoAP.NET/Properties/AssemblyInfo.cs
+++ CoAP.NET/Properties/AssemblyInfo.cs
@@ -24,7 +24,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("SmeshLink Technology")]
 [assembly: AssemblyProduct("CoAP.NET")]
-[assembly: AssemblyCopyright("Copyright © SmeshLink 2011-2014")]
+[assembly: AssemblyCopyright("Copyright © SmeshLink 2011-2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -46,5 +46,5 @@ using System.Runtime.InteropServices;
 // 可以指定所有这些值，也可以使用“内部版本号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.18")]
-[assembly: AssemblyFileVersion("0.18")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
